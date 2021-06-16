bcfd3c6161a32f7a2d913270f0ee332494958aa6
BorisKolbenkov <kolbenkov@gmail.com>
2021-06-16 22:46:47 +0300

    Initial commit

---
+++ main.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main(String[] args)
+    {int account = 2_000_000_000;
+        int add = 500_000_000;
+        int total = account + add;
+        System.out.println(total);
+
+    }
+}
